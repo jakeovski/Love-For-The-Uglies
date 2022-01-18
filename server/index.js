1cdef6c21657a3606678c55335ca25c4a0475a1b
Captain Sir <jakeovski@gmail.com>
2022-01-18 19:35:09 +0000

    Initial Commit Client 0.1.0 Server 0.1.0

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from "mongoose";
+
+//Configure env variables
+dotenv.config();
+
+//Configure Express
+const app = express();
+app.use(express.json({limit:'20mb'}));
+app.use(express.urlencoded({limit:'20mb',extended:true}));
+app.use(cors());
+
+
+//Connect to the database
+mongoose.connect(process.env.CONNECTION_URL)
+    .then(() => app.listen(process.env.SERVER_PORT, () => console.log(`Server started at port ${process.env.SERVER_PORT}`)))
+    .catch((error) => console.log(error));
